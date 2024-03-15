ccab4c413d87a8b32769a45c90e0551e74fbb3f4
Thiha3013 <mminnthiha@gmail.com>
2024-03-14 21:14:29 -0500

    Initial Commit

---
+++ Mummy Maze/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Mummy Maze
+//
+//  Created by Myat Minn Thiha on 3/14/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
